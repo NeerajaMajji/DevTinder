586e59dbd62bb51b8e1aef563c4de1377195db72
Neeraja <neeraja77147714@gmail.com>
2025-08-03 16:01:28 +0530

    feat: routing

--- src/app.js
+++ src/app.js
@@ -3,10 +3,40 @@ console.log("Hello App.js")
 const express= require("express");
 const app=express();
 
-app.listen(7000,()=>{
-    console.log("Server is up and running on 7000 port");
+// routing with regex is only working before 5 version
+
+// c is optional here abcx, abx works here
+app.get("/abc?x",(req,res)=>{
+    res.send("It's ? checking");
+})
+// you can add as many m as you want, abmc, abmmmmc, abmmmmmmmmmc
+app.get("/abm+c",(req,res)=>{
+    res.send("It's + checking");
+})
+// you keep whatever you want in * place, abcmdjgjdjc, abcc, abcmjdc
+app.get("/abc*c",(req,res)=>{
+    res.send("It's * checking");
+})
+// we can ignore both wz ac, awzc
+app.get("/a(wz)?c",(req,res)=>{
+    res.send("It's () checking");
+})
+
+app.get(/a/,(req,res)=>{
+    res.send("It's regex checking");
+})
+
+
+app.post("/hello",(req,res)=>{
+    res.send("It's Post API")
+})
+app.delete("/hello",(req,res)=>{
+    res.send("It's delete API")
+})
+app.use("/hello",(req,res)=>{
+    res.send("Hello from the server!");
 });
 
-app.use((req,res)=>{
-    res.send("Hello from the server");
+app.listen(8000,()=>{
+    console.log("Server is up and running on 7000 port");
 });
