a072c18dfe8c82bc074f0a2e704a9a8f7c1db8d7
Neeraja <neeraja77147714@gmail.com>
2025-08-15 12:57:02 +0530

    feat: added the user signup, delete, patch apis

--- src/app.js
+++ src/app.js
@@ -1,48 +1,85 @@
-console.log("Hello App.js")
+console.log("Hello App.js");
 
-const express= require("express");
-const {AdminAuth}=require("./middlewares/auth");
-const app=express();
+const express = require("express");
+const { AdminAuth } = require("./middlewares/auth");
+const app = express();
 
-const connectionDB= require("./config/database");
+const connectionDB = require("./config/database");
 
-const User=require('./models/user');
+const User = require("./models/user");
 
-app.use('/admin',AdminAuth);
+app.use("/admin", AdminAuth);
+app.use(express.json());
 
-app.post("/signUp",async(req,res)=>{
-const userObj=new User({
-    firstName:'Neeraja',
-    lastName:"Majji",
-    emailId:'neeraja77147714@gmail.com',
-    password:"Neeraja@123"
-})
-try{
-    userObj.save();
-    res.send("Saved data successfully")
-}
-catch(error){
-    res.send("Failed to save the record")
-}
-
-
-})
-
-app.get("/admin/getAllData",(req,res)=>{
-    res.send("Successfully sent all the data");
-})
-
-app.get("/admin/deleteUser",(req,res)=>{
-    res.send("Successfully deleted a user")
-})
-
-connectionDB().then(()=>{
-console.log("DB connection established successfully");
-app.listen(8000,()=>{
-    console.log("Server is up and running on 7000 port");
+app.post("/signUp", async (req, res) => {
+  console.log("req.body", req.body);
+  const userObj = new User(req.body);
+  try {
+    await userObj.save();
+    res.send("Saved data successfully");
+  } catch (error) {
+    res.send("Failed to save the record");
+  }
 });
-}).catch((error)=>{
-console.log("failed to establish the connection")
-})
 
+app.get("/user", async (req, res) => {
+  console.log("req", req.body);
+  const userEmail = req.body.emailId;
+  try {
+    const userData = await User.find({ emailId: userEmail });
+    if (!userData.length) {
+      res.status(404).send("User not found");
+    }
+    res.send(userData);
+  } catch (error) {
+    res.send("Something went wrong");
+  }
+});
 
+app.get("/feed", async (req, res) => {
+  try {
+    const allFeeds = await User.find({});
+    if (!allFeeds.length) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(allFeeds);
+    }
+  } catch (error) {
+    res.send("Something went wrong");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const deletedUser = await User.findByIdAndDelete(userId);
+  res.send("Successfully deleted user", deletedUser);
+});
+
+app.patch("/user", async (req, res) => {
+  const userId = req.body._id;
+  try {
+    const result = await User.findByIdAndUpdate(userId, req.body);
+    res.send(result);
+  } catch (error) {
+    res.send("Something went wrong");
+  }
+});
+
+app.get("/admin/getAllData", (req, res) => {
+  res.send("Successfully sent all the data");
+});
+
+app.get("/admin/deleteUser", (req, res) => {
+  res.send("Successfully deleted a user");
+});
+
+connectionDB()
+  .then(() => {
+    console.log("DB connection established successfully");
+    app.listen(8000, () => {
+      console.log("Server is up and running on 7000 port");
+    });
+  })
+  .catch((error) => {
+    console.log("failed to establish the connection");
+  });
